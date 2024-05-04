9a84f47d12ff5a458e7717626f70af9a724a1ad3
Evgenia Ermakova <mikassik@yandex.ru>
2024-05-04 14:58:29 +0300

    rewiew2.9

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,14 +2,14 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import { useState, useRef, FormEvent } from 'react';
 import {
-  ArticleStateType,
-  defaultArticleState,
-  fontFamilyOptions,
-  fontColors,
-  backgroundColors,
-  contentWidthArr,
-  fontSizeOptions,
-  OptionType
+	ArticleStateType,
+	defaultArticleState,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	OptionType,
 } from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
 import { RadioGroup } from '../radio-group';
@@ -19,87 +19,94 @@ import clsx from 'clsx';
 import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 
 type ArticleParamsFormProps = {
-  params: ArticleStateType;
-  setParams: (params: ArticleStateType) => void;
+	params: ArticleStateType;
+	setParams: (params: ArticleStateType) => void;
 };
 
-export const ArticleParamsForm = ({ params, setParams }: ArticleParamsFormProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+export const ArticleParamsForm = ({
+	params,
+	setParams,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
 
-  const rootRef = useRef<HTMLDivElement | null>(null);
+	const rootRef = useRef<HTMLDivElement | null>(null);
 
-  function handleArrowBtnClick() {
-    setIsOpen(prevState => !prevState);
-  }
-  useOutsideClickClose({
-    isOpen,
-    rootRef,
-    onClose: handleArrowBtnClick,
-    onChange: setIsOpen
-  });
+	function handleArrowBtnClick() {
+		setIsOpen((prevState) => !prevState);
+	}
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: handleArrowBtnClick,
+		onChange: setIsOpen,
+	});
 
-  const [formState, setFormState] = useState(params);
+	const [formState, setFormState] = useState(params);
 
-  const handleOnChange = (key: keyof ArticleStateType) => {
-    return (value: OptionType) => {
-      setFormState(prevState => ({ ...prevState, [key]: value }));
-    };
-  };
+	const handleOnChange = (key: keyof ArticleStateType) => {
+		return (value: OptionType) => {
+			setFormState((prevState) => ({ ...prevState, [key]: value }));
+		};
+	};
 
-  const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setParams(formState);
-  };
+	const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setParams(formState);
+	};
 
-  const handleResetForm = () => {
-    setFormState(defaultArticleState);
-    setParams(defaultArticleState);
-  };
+	const handleResetForm = () => {
+		setFormState(defaultArticleState);
+		setParams(defaultArticleState);
+	};
 
-  return (
-    <div ref={rootRef} className={styles.wrapper}>
-      <ArrowButton onClick={handleArrowBtnClick} isOpen={isOpen} />
-      <aside className={clsx(styles.container, isOpen && styles.container_open)}>
-        <form className={styles.form} onSubmit={handleSubmitForm} onReset={handleResetForm}>
-          <h2 className={styles.title}>Задайте параметры</h2>
-          <Select
-            title={'шрифт'}
-            onChange={handleOnChange('fontFamilyOption')}
-            selected={formState.fontFamilyOption}
-            options={fontFamilyOptions}
-          />
-          <RadioGroup
-            name={'fontSize'}
-            selected={formState.fontSizeOption}
-            options={fontSizeOptions}
-            title={'Размер шрифта'}
-            onChange={handleOnChange('fontSizeOption')}
-          />
-          <Select
-            selected={formState.fontColor}
-            options={fontColors}
-            title={'Цвет шрифта'}
-            onChange={handleOnChange('fontColor')}
-          />
-          <Separator />
-          <Select
-            selected={formState.backgroundColor}
-            options={backgroundColors}
-            title={'Цвет фона'}
-            onChange={handleOnChange('backgroundColor')}
-          />
-          <Select
-            selected={formState.contentWidth}
-            options={contentWidthArr}
-            title={'Ширина контента'}
-            onChange={handleOnChange('contentWidth')}
-          />
-          <div className={styles.bottomContainer}>
-            <Button title="Сбросить" type="reset" />
-            <Button title="Применить" type="submit" />
-          </div>
-        </form>
-      </aside>
-    </div>
-  );
+	return (
+		<div ref={rootRef} className={styles.wrapper}>
+			<ArrowButton onClick={handleArrowBtnClick} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmitForm}
+					onReset={handleResetForm}>
+					<h2 className={styles.title}>Задайте параметры</h2>
+					<Select
+						title={'шрифт'}
+						onChange={handleOnChange('fontFamilyOption')}
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+					/>
+					<RadioGroup
+						name={'fontSize'}
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						title={'Размер шрифта'}
+						onChange={handleOnChange('fontSizeOption')}
+					/>
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						title={'Цвет шрифта'}
+						onChange={handleOnChange('fontColor')}
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						title={'Цвет фона'}
+						onChange={handleOnChange('backgroundColor')}
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						title={'Ширина контента'}
+						onChange={handleOnChange('contentWidth')}
+					/>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' type='reset' />
+						<Button title='Применить' type='submit' />
+					</div>
+				</form>
+			</aside>
+		</div>
+	);
 };
